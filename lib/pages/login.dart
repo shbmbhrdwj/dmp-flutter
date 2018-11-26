666d5034e4f500705f8e7ea6932bf4fe34654203
Shubham Bhardwaj <shbmbhrdwj@gmail.com>
2018-11-26 20:06:10 +0530

    Completed Login UI

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import '../components/text_card.dart';
 import '../components/padded_text_field.dart';
 import '../components/padded_text.dart';
+
 
 class Login extends StatelessWidget {
   String title;
@@ -17,64 +20,82 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(32.0),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextCard(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                        'Login',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.w900
-                        )
-                    ),
-                    PaddedTextField(
-                      labelText: 'Username',
-                      padding: EdgeInsets.all(8.0),
-                    ),
-                    PaddedTextField(
-                      labelText: 'Password',
-                      padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 16.0),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: PaddedText(
-                            text: 'Forgot Password ?',
-                            textAlign: TextAlign.end,
-                            padding: EdgeInsets.all(8.0),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              ButtonBar(
-                children: <Widget>[
-                  RaisedButton(
-                    onPressed: this.onLoginPressed,
-                    color: Colors.blue,
-                    elevation: 16,
-                    child: PaddedText(
-                      text: 'LOGIN',
-                      padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-                      color: Colors.white,
-                    )
-                  )
-                ],
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: AssetImage('assets/loginBg.jpeg'),
+                fit: BoxFit.cover
               )
-            ],
+            ),
+          ),
+          Container(
+              padding: const EdgeInsets.all(32.0),
+              child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextCard(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                                'Login',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                    fontSize: 30.0,
+                                    fontWeight: FontWeight.w900
+                                )
+                            ),
+                            PaddedTextField(
+                              labelText: 'Username',
+                              padding: EdgeInsets.all(8.0),
+                            ),
+                            PaddedTextField(
+                              labelText: 'Password',
+                              padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 16.0),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: PaddedText(
+                                    text: 'Forgot Password ?',
+                                    textAlign: TextAlign.end,
+                                    padding: EdgeInsets.all(8.0),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          RaisedButton(
+                              onPressed: this.onLoginPressed,
+                              color: Colors.blue,
+                              elevation: 16,
+                              shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                              child: PaddedText(
+                                text: 'LOGIN',
+                                padding: EdgeInsets.fromLTRB(48.0, 16.0, 48.0, 16.0),
+                                color: Colors.white,
+                              )
+                          )
+                        ],
+                      ),
+                      PaddedText(
+                        text: 'New User? Signup',
+                        color: Colors.black45,
+                        padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 0.0),
+                      )
+                    ],
+                  )
+              )
           )
-        )
+        ],
       )
     );
   }
