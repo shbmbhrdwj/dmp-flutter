c26c7ff4a8d77739ccc9b16d68c44b6ff2520ff4
Ibrahim Mubarak <ibrahim@faimtech.in>
2018-12-20 19:40:54 +0530

    Complete Built Value Implementation for Category Resonse

--- lib/config/serializers.dart
+++ lib/config/serializers.dart
@@ -7,7 +7,5 @@ import 'package:dmp_flutter/models/response/category_response.dart';
 part 'serializers.g.dart';
 
 @SerializersFor(const [CategoryResponse, Category])
-Serializers serializers = _$serializers;
-
-Serializers standardSerializers =
-    (serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
+final Serializers serializers =
+    (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
--- lib/services/category.dart
+++ lib/services/category.dart
@@ -1,29 +1,18 @@
-import 'dart:convert';
-
 import 'package:dmp_flutter/config/api_client.dart';
-import 'package:dmp_flutter/config/serializers.dart';
 import 'package:dmp_flutter/models/category.dart';
 import 'package:dmp_flutter/models/response/category_response.dart';
 
 class CategoryService {
   static Future<List<Category>> getAll() async {
+    
     final response = await ApiClient.getFrom("/categories");
 
     if (response.statusCode >= 200 && response.statusCode < 400) {
-      print(response.body);
-      var responseJson = json.decode(response.body);
-      // var categories = (responseJson['data']).map((categoryJson) {
-      // print("categories => "+responseJson['data']);
-      // print("category =>"+categoryJson.toString());
-
-      // serializers.deserializeWith(Category.serializer, json.decode(testJson));
       var catResponse = CategoryResponse.fromJson(response.body);
-      print("CatResponse"+catResponse.toString());
-      // }).toList();ß
-      return [];
+      return catResponse.data.asList();
     }
 
     //TODO Handle error states
-    return List();
+    return [];
   }
 }
